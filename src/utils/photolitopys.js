3b596432498bad3f11ae764787c95a6bd7ed9ed7
havriluke <havrylyuk.post@gmail.com>
2023-02-04 13:05:54 +0200

    add photos

--- src/utils/photolitopys.js
+++ src/utils/photolitopys.js
@@ -3,23 +3,17 @@
 export const photolitopys = [
     {
         photos: [
-            'https://i.ibb.co/pQPxsV6/5.png',
-            'https://i.ibb.co/5TnVgt4/photo-1554844453-7ea2a562a6c8.jpg',
-            'https://i.ibb.co/sgycyG1/photo-1517960413843-0aee8e2b3285.jpg',
-            'https://i.ibb.co/HrKfY33/Rotating-earth.gif',
-            'https://i.ibb.co/4mBrCNK/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass-181624-22807.webp',
+            'https://i.ibb.co/g6tBv3G/viber-2021-11-12-13-18-09-011.jpg',
+            'https://i.ibb.co/KKhjNHn/viber-2021-11-12-13-18-09-492.jpg',
+            'https://i.ibb.co/2SgHFZv/viber-2022-11-09-12-03-28-969.jpg',
+            'https://i.ibb.co/19mq1b4/viber-2022-11-09-12-03-29-320.jpg',
+            'https://i.ibb.co/VgmCBCd/viber-2022-11-09-12-03-29-548.jpg',
+            'https://i.ibb.co/h1G5nk7/viber-2022-11-09-12-03-29-993.jpg',
+            'https://i.ibb.co/7YThpxG/1.jpg',
+            'https://i.ibb.co/SmbxmFD/1.png',
+            'https://i.ibb.co/5B8H347/2.png',     
         ],
-        descr: 'Свято весни. Всі велеляться. Настала весна',
-        date: '15 листопада 2018'
-    },
-    {
-        photos: [
-            'https://i.ibb.co/N1pxPRX/340.png',
-            'https://i.ibb.co/9cGGbr4/341.png',
-            'https://i.ibb.co/dPVs7dD/349.png',
-            'https://i.ibb.co/R24Sb4Y/350.png',
-        ],
-        descr: 'Христос воскрес. Воїстину воскрес',
-        date: '20 квітня 2007'
+        descr: 'День української писемності та мови.',
+        date: '9 листопада 2022'
     }
 ]
